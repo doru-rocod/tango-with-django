0f0dca413d3f5065b159663eaee37d51670c33ca
doru-rocod <dorupieloiu@yahoo.com>
2017-03-09 12:03:49 +0200

    after adding about view and url

--- rango/views.py
+++ rango/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Salutare, bostane!")
+    return HttpResponse("Salutare, bostane!<br><a href='/rango/about'>Despre</a>")
+
+
+def about(request):
+    return HttpResponse("Aici bostanul vorbeste despre el<br><a href='/rango/'>Acasa</a>")
