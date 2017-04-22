0a3770e26a692bc9d1478da05c2971eb5ad28d17
doru-rocod <dorupieloiu@yahoo.com>
2017-04-22 12:40:23 +0300

    using sessions

--- rango/views.py
+++ rango/views.py
@@ -13,10 +13,11 @@ from datetime import datetime
 def index(request):
     categories = Category.objects.order_by('-likes')[:5]
     most_viewed_pages = Page.objects.order_by('-views')[:5]
+    visitor_cookie_handler(request)
     context = {'categories': categories,
                'most_viewed_pages': most_viewed_pages}
+    context['visits'] = request.session['visits']
     response = render(request, 'rango/index.html', context)
-    visitor_cookie_handler(request, response)
     return response
 
 
@@ -121,15 +122,24 @@ def user_logout(request):
     return HttpResponseRedirect(reverse('index'))
 
 
-def visitor_cookie_handler(request, response):
-    visits = int(request.COOKIES.get('visits', '1'))
-    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request,
+                                               'last_visit',
+                                               str(datetime.now()))
     last_visit_time = datetime.strptime(last_visit_cookie[:-7],
                                         '%Y-%m-%d %H:%M:%S')
     if (datetime.now() - last_visit_time).seconds > 5:
         visits = visits + 1
-        response.set_cookie('last_visit', str(datetime.now()))
+        request.session['last_visit'] = str(datetime.now())
     else:
         # visits = visits
-        response.set_cookie('last_visit', last_visit_cookie)
-    response.set_cookie('visits', visits)
+        request.session['last_visit'] = last_visit_cookie
+    request.session['visits'] = visits
