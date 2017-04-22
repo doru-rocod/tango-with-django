599f4cb0490cda7a523f5ff3535b5d1380427ce9
doru-rocod <dorupieloiu@yahoo.com>
2017-04-22 09:00:39 +0300

    using cookies

--- rango/views.py
+++ rango/views.py
@@ -7,13 +7,17 @@ from django.core.urlresolvers import reverse
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 
+from datetime import datetime
+
 
 def index(request):
     categories = Category.objects.order_by('-likes')[:5]
     most_viewed_pages = Page.objects.order_by('-views')[:5]
     context = {'categories': categories,
                'most_viewed_pages': most_viewed_pages}
-    return render(request, 'rango/index.html', context)
+    response = render(request, 'rango/index.html', context)
+    visitor_cookie_handler(request, response)
+    return response
 
 
 def show_category(request, slug):
@@ -38,6 +42,7 @@ def add_category(request):
     return render(request, 'rango/add_category.html', {'form': form})
 
 
+@login_required
 def add_page(request, cat_slug):
     try:
         category = Category.objects.get(slug=cat_slug)
@@ -114,3 +119,17 @@ def user_login(request):
 def user_logout(request):
     logout(request)
     return HttpResponseRedirect(reverse('index'))
+
+
+def visitor_cookie_handler(request, response):
+    visits = int(request.COOKIES.get('visits', '1'))
+    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                        '%Y-%m-%d %H:%M:%S')
+    if (datetime.now() - last_visit_time).seconds > 5:
+        visits = visits + 1
+        response.set_cookie('last_visit', str(datetime.now()))
+    else:
+        # visits = visits
+        response.set_cookie('last_visit', last_visit_cookie)
+    response.set_cookie('visits', visits)
